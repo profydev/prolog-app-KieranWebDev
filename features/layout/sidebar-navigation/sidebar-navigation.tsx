ef1bf2137a5ff0960f9aac05b106d4468afc4356
Kieran <kieran.webdevelopment@gmail.com>
2023-11-13 13:46:34 +0000

    fixed the bug, but uses useEffect. may be better solution

--- features/layout/sidebar-navigation/sidebar-navigation.tsx
+++ features/layout/sidebar-navigation/sidebar-navigation.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Routes } from "@config/routes";
 import classNames from "classnames";
 import { NavigationContext } from "./navigation-context";
@@ -20,6 +20,19 @@ export function SidebarNavigation() {
   const router = useRouter();
   const { isSidebarCollapsed, toggleSidebar } = useContext(NavigationContext);
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const [windowLength, setWindowLength] = useState(0);
+
+  useEffect(() => {
+    function handleResize() {
+      setWindowLength(window.innerWidth);
+    }
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  console.log(windowLength);
   return (
     <div
       className={classNames(
@@ -37,7 +50,7 @@ export function SidebarNavigation() {
           {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={
-              isSidebarCollapsed
+              isSidebarCollapsed && windowLength > 1020
                 ? "/icons/logo-small.svg"
                 : "/icons/logo-large.svg"
             }
